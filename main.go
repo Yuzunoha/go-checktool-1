5395d738a380b4cc459bc438304da1e075501bed
Yuzunoha <studysteadystandby@gmail.com>
2022-05-28 03:56:31 +0900

    postできた

--- main.go
+++ main.go
@@ -3,16 +3,49 @@ package main
 import (
   "fmt"
   "net/http"
+  "net/url"
   "io/ioutil"
+  "log"
+  "io"
 )
 
-
 func main() {
-  url := "http://google.co.jp"
+	sub2();
+}
 
-  resp, _ := http.Get(url)
-  defer resp.Body.Close()
+func sub1() {
+	url := "https://markdown.yuzunoha.net/go"
 
-  byteArray, _ := ioutil.ReadAll(resp.Body)
-  fmt.Println(string(byteArray)) // htmlをstringで取得
+	resp, _ := http.Get(url)
+	defer resp.Body.Close()
+  
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(byteArray)) // htmlをstringで取得
+}
+
+func sub2() {
+    // POSTメソッド
+    // url.Values{}でPOSTで送信する入れ物を準備
+    ps := url.Values{}
+
+    // Add()でPOSTで送信するデータを作成
+    ps.Add("id", "1")
+    ps.Add("name", "もりぴ")
+
+    // 特殊文字や日本語をエンコード
+    fmt.Println(ps.Encode())
+
+    // http.PostForm()でPOSTメソッドを発行
+    res, err := http.PostForm("https://markdown.yuzunoha.net/api/go", ps)
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // deferでクローズ処理
+    defer res.Body.Close()
+    // Bodyの内容を読み込む
+    body, _ := io.ReadAll(res.Body)
+    // Bodyの内容を出力する
+    fmt.Print(string(body))
 }
