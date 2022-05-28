c91dc88e65bb2d0a80b0135862a80d66878694a3
Yuzunoha <studysteadystandby@gmail.com>
2022-05-29 02:39:36 +0900

    手元で動いた

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
   "fmt"
   "net/http"
-  "net/url"
   "io/ioutil"
   "log"
   "io"
@@ -78,34 +77,6 @@ func sub1() {
   
 	byteArray, _ := ioutil.ReadAll(resp.Body)
 	fmt.Println(string(byteArray)) // htmlをstringで取得
-}
-
-func sub2(argUrlStr string) {
-    // url.Values{}でPOSTで送信する入れ物を準備
-    ps := url.Values{}
-
-    // Add()でPOSTで送信するデータを作成
-    ps.Add("id", "1")
-    ps.Add("name", "もりぴ")
-
-    // 特殊文字や日本語をエンコード
-    fmt.Println(ps.Encode())
-
-    // http.PostForm()でPOSTメソッドを発行
-    res, err := http.PostForm(argUrlStr, ps)
-
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    // deferでクローズ処理
-    defer res.Body.Close()
-    
-	// Bodyの内容を読み込む
-    body, _ := io.ReadAll(res.Body)
-    
-	// Bodyの内容を出力する
-    fmt.Print(string(body))
 }
 
 func SendPostRequestOld(url string, filename string) (string, []byte) {
