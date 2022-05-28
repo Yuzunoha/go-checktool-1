4b7fc6ec903531daa6390b14d4c1f1d0f58649a9
Yuzunoha <studysteadystandby@gmail.com>
2022-05-29 03:40:02 +0900

    コメント書いた

--- main.go
+++ main.go
@@ -12,6 +12,8 @@ import (
   "path/filepath"    
 )
 
+var p = fmt.Println
+
 func main() {
 	// 送信先のurl
 	var toUrlStr = "https://markdown.yuzunoha.net/api/go"
@@ -36,25 +38,31 @@ func SendPostRequest (url string, filePath string, fieldName string) []byte {
 	body := &bytes.Buffer{}
 	// MultipartWriterを作る。boundaryは自動で設定される。
     writer := multipart.NewWriter(body)
-	// 送信するフォームを作る
+	// フォームを作る。右辺の引数はそれぞれ "file", "test.txt"
     part, err := writer.CreateFormFile(fieldName, filepath.Base(file.Name()))
     if err != nil {
         log.Fatal(err)
     }
-
+	// フォームに送信するファイルの中身をコピーする
     io.Copy(part, file)
+	// MultipartWriterを閉じる
     writer.Close()
+	// リクエストを作成する。ペイロードはバッファ領域へのポインタ「body」を指定することに注意
     request, err := http.NewRequest("POST", url, body)
     if err != nil {
         log.Fatal(err)
     }
+	// リクエストヘッダを書き込む。値は "multipart/form-data; boundary=aab16..."
     request.Header.Add("Content-Type", writer.FormDataContentType())
+	// クライアントのポインタを取得する
     client := &http.Client{}
+	// リクエストを実行してレスポンスを受け取る
     response, err := client.Do(request)
     if err != nil {
         log.Fatal(err)
     }
     defer response.Body.Close()
+	// レスポンスからコンテンツを抽出して返却する。おわり
     content, err := ioutil.ReadAll(response.Body)
     if err != nil {
         log.Fatal(err)
