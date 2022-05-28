eeffdb71e970a500e60d5a43431170e4af8630f4
Yuzunoha <studysteadystandby@gmail.com>
2022-05-29 03:31:30 +0900

    整理中

--- main.go
+++ main.go
@@ -26,17 +26,18 @@ func main() {
 }
 
 func SendPostRequest (url string, filePath string, fieldName string) []byte {
+	// フルパス指定で送信するファイルを開く
     file, err := os.Open(filePath)
-
     if err != nil {
         log.Fatal(err)
     }
     defer file.Close()
-
+	// 送信のためのバッファ領域を確保する
 	body := &bytes.Buffer{}
+	// MultipartWriterを作る。boundaryは自動で設定される。
     writer := multipart.NewWriter(body)
+	// 送信するフォームを作る
     part, err := writer.CreateFormFile(fieldName, filepath.Base(file.Name()))
-
     if err != nil {
         log.Fatal(err)
     }
@@ -44,26 +45,19 @@ func SendPostRequest (url string, filePath string, fieldName string) []byte {
     io.Copy(part, file)
     writer.Close()
     request, err := http.NewRequest("POST", url, body)
-
     if err != nil {
         log.Fatal(err)
     }
-
     request.Header.Add("Content-Type", writer.FormDataContentType())
     client := &http.Client{}
-
     response, err := client.Do(request)
-
     if err != nil {
         log.Fatal(err)
     }
     defer response.Body.Close()
-
     content, err := ioutil.ReadAll(response.Body)
-
     if err != nil {
         log.Fatal(err)
     }
-
     return content
 }
